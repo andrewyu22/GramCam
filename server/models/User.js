1933de85b791d6c1412c6ebfdc9138f7b626e736
Brad <kingdomlandscapevt@gmail.com>
2022-03-08 15:17:04 -0500

    updated user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,14 @@
-const { Schema, model } = require('mongoose');
-const bcrypt = require('bcrypt');
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+const dateFormat = require("../utils/dateFormat");
 
-const postSchema = require('./Post');
-const commentsSchema - require('./Comment.js');
+const postSchema = require("./Post");
+const commentsSchema = require("./Comment.js");
 
 const userSchema = new Schema(
   {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
     username: {
       type: String,
       required: true,
@@ -15,14 +18,28 @@ const userSchema = new Schema(
       type: String,
       required: true,
       unique: true,
-      match: [/.+@.+\..+/, 'Must use a valid email address'],
+      match: [/.+@.+\..+/, "Must use a valid email address"],
     },
     password: {
       type: String,
       required: true,
     },
-    posts: [postSchema],
-	comments: [commentsSchema]
+    avatarImg: {
+      type: String,
+      default:
+        "https://cdn2.iconfinder.com/data/icons/mixed-communication-and-ui-pack/48/general_pack_NEW__user-512.png",
+    },
+    posts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+    created_at: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
   },
   {
     toJSON: {
@@ -31,8 +48,8 @@ const userSchema = new Schema(
   }
 );
 
-userSchema.pre('save', async function (next) {
-  if (this.isNew || this.isModified('password')) {
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
   }
@@ -44,10 +61,6 @@ userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-userSchema.virtual('postCount').get(function () {
-  return this.posts.length;
-});
-
-const User = model('User', userSchema);
+const User = model("User", userSchema);
 
 module.exports = User;
