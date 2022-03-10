53daf85c4fd057a75f6ead3ffa7b791afd38b4ad
Brad <kingdomlandscapevt@gmail.com>
2022-03-09 20:12:10 -0500

    merging

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -16,33 +16,33 @@ const resolvers = {
     user: async (parent, { username }, context) => {
       if (context.user) {
         const userData = await User.findOne({ username })
-        .select("-__v -password")
-        .populate("posts");
+          .select("-__v -password")
+          .populate("posts");
         return userData;
       }
       throw new AuthenticationError("You need to be logged in.");
     },
   },
   Mutation: {
-      newUser: async (parent, args) => {
-          const newUser = await User.create(args);
-          const token = signToken(newUser);
+    newUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
 
-          return { token, newUser};
-      },
-      login: async (parent, {email, password}) => {
-          const user = await User.findOne({email});
-          if(!user) {
-            throw new AuthenticationError("Invaild user.");
-          }
-          const checkPassword = await user.isCorrectPassword(password)
-          if(!checkPassword) {
-            throw new AuthenticationError("Invalid password.");
-          }
-          const token = signToken(user)
-          return { token, user};
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError("Invaild user.");
       }
-  }
+      const checkPassword = await user.isCorrectPassword(password);
+      if (!checkPassword) {
+        throw new AuthenticationError("Invalid password.");
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
 };
 
 module.exports = resolvers;
