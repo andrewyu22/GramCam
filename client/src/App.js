50680e7ce05a75ed35b19bef737f5ba36240c22d
Mario Villaquiran <mariovillaquiran@marios-air.myfiosgateway.com>
2022-03-10 13:23:21 -0500

    installed apollo client to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,43 @@
 import "./App.css";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import Nav from "./components/Nav";
 import Signup from "./components/Signup";
 
+const httpLink = createHttpLink({
+  uri: '/graphql'
+});
+
+const authLink = setContext((_, { headers}) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '';
+    }
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
 function App() {
   return (
+    <ApolloProvider client={client}>
     <div className="App">
       <header className="App-header">
         <Nav></Nav>
       </header>
       <Signup />
     </div>
+    </ApolloProvider>
+    
   );
 }
 
