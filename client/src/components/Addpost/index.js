1269add9781b73988a89f0fbc047f97a822ad65c
Andrew Yu <andrewyu209@gmail.com>
2022-03-12 23:52:22 -0500

    Upload Image to the firebase storage, get the URL and useMutation ADD_POST

--- client/src/components/Addpost/index.js
+++ client/src/components/Addpost/index.js
@@ -1,8 +1,14 @@
 import React, { useState } from "react";
+import { storage } from "../../utils/firebase";
+import { ref, getDownloadURL, uploadString } from "firebase/storage";
+import { useMutation } from "@apollo/client";
+import { ADD_POST } from "../../utils/mutations";
 
 function Addpost() {
   const [selectedImg, setSelectedImg] = useState(null);
+  const [Img, setImg] = useState(null);
   const [caption, setCaption] = useState("");
+  const [newPost] = useMutation(ADD_POST);
 
   const handleInputChange = (event) => {
     setCaption(event.target.value);
@@ -13,12 +19,35 @@ function Addpost() {
     const reader = new FileReader();
     if (e.target.files[0]) {
       reader.readAsDataURL(e.target.files[0]);
+      setImg(e.target.files[0]);
     }
     reader.onload = (readerEvent) => {
       setSelectedImg(readerEvent.target.result);
     };
   };
 
+  const addPost = async () => {
+    const uploadImg = ref(storage, `/image/${Img.name}`);
+    try {
+      await uploadString(uploadImg, selectedImg, "data_url").then(
+        async (snapshot) => {
+          const downloadUrl = await getDownloadURL(uploadImg);
+          console.log(downloadUrl);
+
+          await newPost({
+            variables: { postImg: downloadUrl, caption: caption },
+          });
+
+          setCaption("");
+          setImg(null);
+          setSelectedImg(null);
+          // window.location.reload();
+        }
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div
       class="modal fade"
@@ -68,10 +97,12 @@ function Addpost() {
           </div>
           <div class="modal-footer">
             <button
+              disabled={!selectedImg}
               data-mdb-toggle="modal"
               data-mdb-target="#addPostModal"
               type="button"
               class="btn btn-primary"
+              onClick={addPost}
             >
               Add Post
             </button>
